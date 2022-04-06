403425def56644d286fdd2340f665eb014496c3d
ViniciusLisboa07 <viniciuslisboa1001@gmail.com>
2022-04-06 20:19:16 -0300

    service methods

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -16,12 +16,20 @@ export class ProductService {
     return this.http.get<Product[]>(`${this.baseUrl}`);
   }
 
-
   getById(id: number): Observable<Product> {
     return this.http.get<Product>(`${this.baseUrl}/${id}`);
   }
 
-//   create(product: Product) : Observable<Product> {
-//     return this.http.post<Product>(`${this.baseUrl}/create`, product);
-//   }
+  create(product: Product) : Observable<Product> {
+    return this.http.post<Product>(`${this.baseUrl}`, product);
+  }
+
+  update(product: Product, id:  number) : Observable<Product> {
+    return this.http.put<Product>(`${this.baseUrl}/${id}`, product);
+  }
+
+  deletetById(id: number): Observable<Product> {
+    return this.http.delete<Product>(`${this.baseUrl}/${id}`);
+  }
+
 }
